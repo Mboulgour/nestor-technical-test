155432b7323c97a201d123d0b3bf2198b811a882
Mboulgour <mathieu.r.77@gmail.com>
2019-11-22 11:31:34 +0100

    add styling to cards + add fontawesome

--- src/components/Rooms.js
+++ src/components/Rooms.js
@@ -1,17 +1,27 @@
 import React, { useState, useEffect } from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+
 import './styles/Rooms.scss';
 
 const Rooms = () => {
   const [rooms, setRooms] = useState([]);
+  const [clients, setClients] = useState([]);
 
   useEffect(() => {
-    const fetchData = () => {
+    const fetchRoomsData = () => {
       fetch('https://technical-test-api.herokuapp.com/rooms')
       .then(result => result.json())
       .then(data => setRooms(data))
     };
-    fetchData();
+    const fetchClientsData = () => {
+      fetch('https://technical-test-api.herokuapp.com/clients')
+      .then(result => result.json())
+      .then(data => setClients(data))
+    };
+    fetchRoomsData();
+    fetchClientsData();
   }, []);
 
   const distinctCities = [...new Set(rooms.map(room => room.city))];
@@ -19,16 +29,37 @@ const Rooms = () => {
   return(
     <div className="room__container">
       {distinctCities.map(city => (
-        <div classname='room__city_container'>
+        <div className="room__city_container">
           <h1 className="room__city__name">{city}</h1>
-          {rooms.filter(r => city === r.city).map(room =>(
-            <div className="room__card" key={room.id}>
-              <div>{room.name}</div>
-              <div>{room.address}</div>
-              <div>{room.zip}</div>
-              <div>{room.status}</div>
-            </div>
-          ))}
+          <div className="room__cards">
+            {rooms.filter(r => city === r.city).map(room =>(
+              <div className="room__card" key={room.id}>
+                <div className="room__card_header">
+                  <FontAwesomeIcon icon={faHome} />
+                  <div className="room__card_props">
+                    <span className="room__card_props--address">{room.address}, {room.zip}</span>
+                    <span className="room__card_props--name">{room.name}</span>
+                  </div>
+                </div>
+                <div className="room__card_body">
+                  <p className="room__card_status">
+                    <span className= {room.status === 'occupied' ? "room__status_badge room__status--occupied" : "room__status_badge room__status--available"}></span>
+                    {room.status}
+                  </p>
+                  {clients.filter(client => client.roomId === room.id).length > 0 
+                    ?
+                    clients.filter(client => client.roomId === room.id).map(client => 
+                      <div className="room__card_user">
+                        {client.name}
+                      </div> 
+                      )
+                    : 
+                    null
+                  }
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       ))}
     </div>
